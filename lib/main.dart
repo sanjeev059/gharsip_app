a8b9f4715e808084b344bfc92e4a6d808017f30c
sanjeev059 <sanjeevdesai538@gmail.com>
2026-05-23 15:20:59 +0530

    feat: add Firebase   config and wire DefaultFirebaseOptions

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'firebase_options.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const GharsipApp());
 }
